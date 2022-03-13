92ac108cfd6eb97c22626419b92efa13e9601db3
MuhammadZubair786 <mzhassan444@gmail.com>
2022-03-14 02:58:59 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,61 @@
+void main(){
+  String data ="Muhammad Ali";
+  String data1 = "MAlIk";
+  print(data.compareTo(data1));
+  print(data.split(" "));  //return data in list
+  var d = data.split(" ");
+  print(d[0]);
+  print(d[1]);
+
+  // 0=>both same
+// 1=>when first big ascii code
+// -1=>when second ascii code capital
+                // 0 1 2 3 4 5 6 
+  // string data store in  index=>start with 0
+  // print(data[0]);
+ 
+//   print(data.toUpperCase());//string
+//   print(data.toLowerCase());//string
+// print(data.indexOf(" ",4));//int
+// print(data.substring(4));//string
+// 4,7
+
+// print(data[0].toUpperCase()+data.substring(1,8).toLowerCase()
+//                 +data[8].toUpperCase()+data.substring(9).toLowerCase());
+// print(newString);
+
+// print(data.length-1);
+// print(data[1]);
+
+// print(data.codeUnits);//return all character ascii code
+// print(data.codeUnitAt(0)); //return  spefic ascii code of any index number
+// print(data.startsWith("zBT"));//return bool value 
+// print(data.endsWith("123"));//return bool
+
+// print(data.contains("t")); //return bool value
+
+// 0=>both same
+// 1
+// -1
+
+
+
+
+
+
+// substring(start index,endindex-1)
+
+  
+ 
+
+
+  // ASad
+  // asad
+  // 65
+  // asaD
+  //97
+  // asad
+
+  //Ascii Code=>
+
+}
