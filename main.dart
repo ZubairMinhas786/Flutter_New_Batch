2d81198617e4a5ff4be3c88ae629fe4b4b70710b
MuhammadZubair786 <mzhassan444@gmail.com>
2022-03-14 03:01:24 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+void main(){
+  // var a =["asasd","ali","omer","zebtech","smit"];
+  var a =[12.3,45,5667,789,6464,676];
+  // List a=["li","zebtech"];
+  // List b2 =["aggaga","ahahhaha","ahggaga",12];
+  // print(a[0]);//access first ELEMENT
+  // print(a.length);
+  // print(a[a.length-1]);access last Element
+  // print(a);
+
+  // a.sort(); //data sort in ascending order
+  // print(a);
+  // print(a.reversed);
+// var b = List.of(a.reversed);
+// b.sort();
+// print(b2.reversed);
+// print(a);
+// a.add(23);
+// print(a);
+// a.add("Hyderabad");
+// print(a);
+// a.add([1,23,44]);
+// a.add(12);
+// print(a);
+// var data = (stdin.readLineSync()!);
+// a.add(data);
+// print(a);
+
+// a.addAll([12,34,56,[78,56],78]);
+// print(a[a.length-2]);
+
+// print(a.first);
+// print(a.last);
+
+// print(a.elementAt(3));
+// a.replaceRange(2, 6, ["smit hyd","faith","zebtech"]);
+// print(a);
+
+// a.replaceRange(2, 4, [2,23,45,56]);
+// print(a);
+
+
+// var b =a.firstWhere((el) => el =="asad");
+// print(b);
+// print(a.contains(112121));
+
+// print(a.skip(4));
+// print(a.take(4).toList());
+
+
+// print(a.any((element) => element.startsWith("a")));
+
+List b =["B","C",""];
+
+// print(a.getRange(2, 5));
+// a.setRange(2, 6, [12,23,4]);
+// print(a);
+
+
+a.setRange(3, 4, [230,34]);
+print(a);
+}
