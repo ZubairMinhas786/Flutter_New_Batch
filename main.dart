5f8fc2953e6cda00e660dda56fce5b70d380dc49
MuhammadZubair786 <mzhassan444@gmail.com>
2022-03-14 03:02:29 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,60 @@
+void main() {
+  // var c = a[0];
+  // var b = a.fold(a.first,
+  //  (previousValue, element)=> 
+  //  element > 5 
+  // ? 
+  // element
+  // :
+  // previousValue
+  
+  // );
+  // print(b);
+  // previoousValue=>23
+  // element=>5
+  // 5>23 =>  23
+
+
+
+
+  // tenary OPERATOR ? :
+// var   b = "Bsad";
+// b.startsWith("a") ?  print("Data")  :  print("No");
+
+// map=>dictioanry=>key value pair
+// var data ={
+//   "name":"ali"
+// };
+var a = [1, 2,5, 4, 2, 5,67,39,56,67];
+
+// python=>set,tuples
+// set=>unique value
+
+// (),{}=>set
+// a.fillRange(2,6,23);
+// print(a);
+
+// a.forEach((element) {
+//   print("daat : ${element.toString()}");
+  
+//   });
+
+// a.clear();
+var b =a.toSet();
+print(b);
+
+List c2=[1,23,[3,45]];
+print(c2[2][1]);
+
+
+// var b = a.map((e) => {
+//   print(e)});
+
+// // print(b);
+ 
+}
+
+// previous Value=> 10
+// element=>5
+
+// 10+5=>15
