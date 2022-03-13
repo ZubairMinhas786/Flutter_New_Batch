7555c11fc451b8b0f3a6f5431cfb431d0f343883
MuhammadZubair786 <mzhassan444@gmail.com>
2022-03-14 02:55:13 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+void  main() {
+  
+
+  // stdout.write("Enter Fist Number  :  ");
+  // // print("Enter Your Name"); //get  line and next move new line
+  // var a = int.parse(stdin.readLineSync()!);//use for getting input from user
+  // // print("Your name ${a} type : ${a.runtimeType}");
+  // stdout.write("Enter Second Number : ");
+  // var b = int.parse(stdin.readLineSync()!);
+  // print("value Of a :${a}");
+  // print("value Of b :${b}");
+  // print("Reminder of a and b is : ${a % b}");
+
+
+  // var a =stdin.readLineSync();
+
+  // print(a);
+
+
+
+}
