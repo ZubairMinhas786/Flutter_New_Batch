2cdd9458d7e1b3f76bbc77eab98ed96d257e01c8
MuhammadZubair786 <mzhassan444@gmail.com>
2022-03-14 03:00:06 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+
+void main(){
+  // var s = "jshhjsjd";
+  // var d=s.replaceFirst(from, to)
+  // replaceRange(start,end-1)
+  // print(s.replaceAll("e", "12345hbgxxhgzhd"));/
+  // var d = s.replaceRange(0,s.length,"Ali");
+  // print(d);
+  // print(s.)
+  // print(s.isNotEmpty);
+
+  // switch case=>
+  // syntax
+  // switch(varaiable){
+    // cases
+    // statements
+  // }
+
+  // var a =20;
+  
+  // switch(a){
+  //   case 0 :
+  //   print("Sunday"); 
+  //     break;
+
+  //   case 10:
+  //   print("Monday");
+  //   break;
+
+  //   case 2:
+  //   print("Tuesday");
+  //   break;
+
+  //   default:
+  //   print("Incorrect data");
+  // }
+
+
+// 0,1,,other value
+// stdout.write("Enter Data ");
+//   var a = int.parse(stdin.readLineSync()!);
+
+//   switch(a){
+//     case 0 :
+//     print("Tail");
+//     print("Last ");
+    
+//     break;
+   
+
+//     case 1:
+//     print("Head");
+//     break;
+
+//     default:
+//     print("Incorrect Data");
+//   }
+
+// 1 to 5=>ticket free or lunch free
+// 6 to 10=>ticket free or not lunch free
+// 11 to 15=>buy everything
+// 16=>no seat for you
+
+// var age=int.parse(stdin.readLineSync()!);
+
+// switch(age){
+//   case 1:
+//   case 2:
+//   case 3:
+//   case 4:
+//   case 5:
+//   print("ticket free or lunch free");
+//   break;
+
+//   case 6:
+//   case 7:
+//   case 8:
+//   case 9:
+//   case 10:
+//   print("ticket free or lunch not");
+//   break;
+
+//   case 11:
+//   case 12:
+//   case 13:
+//   case 14:
+//   case 15:
+//   print("buy everything");
+//   break;
+
+//   default:
+//   print("No Seat For You");
+
+
+
+// }
+
+
+// user input=> 2180
+
+
+
+
+// 2180~/500=>4=>180
+// 500=>4
+// 100=>1
+// 50=>1
+// 10=>3
+  
+
+  // int a = int.parse(stdin.readLineSync()!);
+
+  // var fiveh = a~/500;   //how many note of 500 h
+  // var fivel = a%500;//how many price left from user input
+
+  // var hundh=fivel~/100;
+  // var hundl = fivel%100; 
+
+  // var fityh =  hundl~/50;
+  // var fityl = hundl%50;
+
+  // var tenh=fityl~/10;
+  // var tenhl=fityl%10;
+
+  // print(fiveh);
+  // print(fivel);
+
+  // print(hundh);
+  // print(hundl);
+
+  // print(fityh);
+  // print(fityl);
+
+  // print(tenh);
+  // print(tenhl);
+
+List a=[12,12,true];
+
+  print(a);
+
+
+
+
+  }
