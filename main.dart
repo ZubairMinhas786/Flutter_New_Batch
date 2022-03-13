dc1f3f36e01c1a5e3946b042690352c0b3127674
MuhammadZubair786 <mzhassan444@gmail.com>
2022-03-14 02:51:22 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,78 @@
+void main(){
+  // escape:\n\t\*
+  // syntax
+  // datatype varaiblename=value;
+  // double name = 34.78;
+//  bool name =true;
+// print(name );
+// print(name );
+// print(name );
+// print(name );
+int a1 =10;
+double a2 = 34.78;
+String course = "Flutter";
+bool enroll = true;
+
+
+// conctaentaion=>use for join string with varaible
+// print(a1.toString()+ a2.toString()+course+enroll.toString());
+
+// interploation
+// print("${a1}${a2} ${course} ${enroll} ${}");
+// double $n1=1123;
+// String a ="12";
+// print(a.runtimeType);
+// ;=>statement terminator
+// String name ="asad";
+// num a= 2020 ;
+// num b=4;
+// num c = a % b;
+// print(c);
+
+// output=>
+// Sum of 12 and 10 is => 22
+// print("The Multiplication of "+a.toString()+ "and"+ b.toString() + ":" + c.toString());
+
+// print("The Multiplication  of ${a} and ${b} is : ${c}");
+// num a =10;
+// print(a++); //in =>11
+// print(a);
+var a=10;
+var b =4;
+
+// print(--a);
+// print(a);
+// var c = a++ + ++b + a++ - b++;
+        // 10 +  5 +  11 - 5;
+// print(c); //9
+// 12
+// 14
+// 19
+// 21,21,21,21
+
+var c = a++ + ++a +  b-- +  --b +  a++ + b-- + ++a -  a++ + --b ;
+//      42  
+// 22,22,22,22
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
