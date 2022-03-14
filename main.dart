b10e8dba88d046829f7ffcfc83cec3480e3281b7
MuhammadZubair786 <mzhassan444@gmail.com>
2022-03-14 18:30:01 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+void main() {
+// var pairs = [[1, 2], [3, 4]];
+// var flattened = pairs.expand((pair) => pair).toList();
+// print(flattened);
+
+  // var UserData = {
+  //   "name1": "asad",
+  //     "2": 23};
+// print(UserData.runtimeType);
+// print(UserData.keys);
+// print(UserData.values);
+// print(UserData[2]);
+
+// UserData["name"]="ali";
+// print(UserData);
+
+// UserData.putIfAbsent("name2", () => "ali");
+// print(UserData);
+
+// print(UserData.length);
+
+// print(UserData.containsKey("name"));
+  // stdout.write("Enter key : ");
+  // var k = stdin.readLineSync()!;
+
+  // stdout.write("Enter Value : ");
+  // var v = stdin.readLineSync()!;
+
+  // if (UserData.containsKey(k)) {
+  //   print("already Added");
+  // } else {
+  //   UserData[k] = v;
+  // }
+  // print(UserData);
+
+//  var b =  UserData.containsValue("23");
+//  print(b);
+
+// var UserData = {
+//     "name1": "asad",
+//     2: 23};
+//       print(UserData.runtimeType);
+
+//   UserData.addAll({
+//     "name1": true,
+//     23:"newsata"
+
+//   });
+//   print(UserData);
+
+  var userData1 = [
+    {"id": 1, "name": "asad", "marks": 200, "Trade": "Android"},
+    {"name": "asad", "marks": 150, "Trade": ["Cit","DcBm","Web aPP"]},
+    {"name": "omer", "marks": 100, "Trade": "Android"}
+  ];
+
+print(userData1);
+
+}
+
+
