1e28b463e5bc716f7f662bd447dcb2e3f0eb8802
MuhammadZubair786 <mzhassan444@gmail.com>
2022-03-17 18:01:18 +0500

    New data added

---
+++ main.dart
@@ -0,0 +1,42 @@
+void main(){
+  
+  // var s = {1,2,3,4,65,"asad"};
+  // print(s.runtimeType);
+  // var data = {"Ai","Andriod","ai"};
+  // print(data);
+
+
+var s = {23,23,23,21,19};
+// 23+21=>44+19=>63
+// print(s.length);
+// print(s.last);
+
+// print(s.single); //check only 1 ele or not
+// print(s.elementAt(1)); //elemetAt(indexnumber)
+
+// print(s.add(12)); //add if not present in set
+// print(s);
+
+
+// s.addAll({23,23,23,23,23});
+// print(s);
+
+
+
+// print(s.any((element) => element ==  21)); //any one same
+// print(s.every((element) => element==23 || element== 20 || element==19));//all value same
+// print(s.contains(20));
+// print(s.toList()); //change into list
+// print(s.containsAll([23,21,19]));
+
+// print(s.firstWhere ((element) => element==23 || element==21));
+// print(s.fold(0, (previousValue, element) => int.parse(previousValue.toString())+element));
+
+var s1 = {23,12,45};
+var s2 = {23,21,19};
+print(s1.difference(s2)); //12,45
+
+
+
+
+}
